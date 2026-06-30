a58ca368ab53a57ea7305213f0d084434a9ee327
Abhijithmns <abhijithmns07@gmail.com>
2026-07-01 00:25:56 +0530

    basic namespace implemetation(uts,pid,mount)

--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// docker         run image <cmd> <params>
+// go run main.go run       <cmd> <params>
 
 func main() {
-	fmt.Println("hollow")
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		panic("bad command usage")
+	}
 }
+
+func run() {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command("/proc/self/exe",append([]string{"child"},os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	// create a child process (namespaces)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS, // because mount by default shares it to the host (other namespaces) 
+	}
+	cmd.Run()
+
+}
+
+func child() {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+
+	syscall.Sethostname([]byte("hollow"))
+	must(syscall.Chroot("./rootfs"))
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "") // container should be mouted to its own version of /proc
+
+	cmd := exec.Command(os.Args[2],os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	cmd.Run()
+
+	syscall.Unmount("proc", 0)
+}
+
+
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
