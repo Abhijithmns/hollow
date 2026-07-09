d886a0f1deec49a5192dd66a2c92047ec6271c60
Abhijithmns <abhijithmns07@gmail.com>
2026-07-09 11:24:16 +0530

    feat: update operation for kill

--- internal/operations/kill.go
+++ internal/operations/kill.go
@@ -2,6 +2,10 @@ package operations
 
 import (
 	"fmt"
+	"strconv"
+
+	"github.com/Abhijithmns/hollow/internal/container"
+	"golang.org/x/sys/unix"
 )
 
 // Kill <container-id> <signal>
@@ -12,7 +16,23 @@ type KillOpts struct {
 }
 
 func Kill(opts *KillOpts) error {
-	fmt.Println(opts)
+	cont, err := container.Load(opts.ID)
+	if err != nil {
+		return fmt.Errorf("load container: %w", err)
+	}
+	sig , err := strconv.Atoi(opts.Signal) // signal is passed via CLI so it'll be a string
+	if err != nil {
+		return fmt.Errorf("convert signal to int: %w", err)
+	}
+
+	if err := cont.Kill(unix.Signal(sig)); err != nil {
+		return fmt.Errorf("kill container: %w", err)
+	}
+
+	if err := cont.Save(); err != nil {
+		return fmt.Errorf("save container: %w", err)
+	}
+
 
 	return nil
 }
