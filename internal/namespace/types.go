9220f74b13a67398bb1504fa35e3262957c3ec1d
Abhijithmns <abhijithmns07@gmail.com>
2026-07-14 00:37:37 +0530

    feat: set up variables for clone flags

---
+++ internal/namespace/types.go
@@ -0,0 +1,32 @@
+package namespace
+
+import (
+	"github.com/opencontainers/runtime-spec/specs-go"
+	"golang.org/x/sys/unix"
+)
+
+// This maps LinuxNamespaceType to corresponding Linux clone flags
+var NamespaceFlags = map[specs.LinuxNamespaceType]uintptr {
+	specs.PIDNamespace : unix.CLONE_NEWPID,
+	specs.NetworkNamespace : unix.CLONE_NEWNET,
+	specs.MountNamespace : unix.CLONE_NEWNS,
+	specs.IPCNamespace : unix.CLONE_NEWIPC,
+	specs.UTSNamespace : unix.CLONE_NEWUTS,
+	specs.UserNamespace : unix.CLONE_NEWUSER,
+	specs.CgroupNamespace : unix.CLONE_NEWCGROUP,
+	specs.TimeNamespace : unix.CLONE_NEWTIME,
+}
+
+// This maps LinuxNamespaceType to corresponding env flag names
+var NamespaceEnvs = map[specs.LinuxNamespaceType]string {
+	specs.PIDNamespace : "pid",
+	specs.NetworkNamespace : "net",
+	specs.MountNamespace : "mnt",
+	specs.IPCNamespace : "ipc",
+	specs.UTSNamespace : "uts",
+	specs.UserNamespace : "user",
+	specs.CgroupNamespace : "cgroup",
+	specs.TimeNamespace : "time",
+}
+
+
